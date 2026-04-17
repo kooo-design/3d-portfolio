99e98fce5d7d37f51ae58cd5ccecbe7f82a98d31
rckyrrr <rickyrohaendi2@gmail.com>
2026-04-17 14:19:27 +0700

    first

---
+++ src/constants/index.js
@@ -0,0 +1,298 @@
+const navLinks = [
+  {
+    name: "Work",
+    link: "#work",
+  },
+  {
+    name: "Experience",
+    link: "#experience",
+  },
+  {
+    name: "Skills",
+    link: "#skills",
+  },
+  {
+    name: "Testimonials",
+    link: "#testimonials",
+  },
+];
+
+const words = [
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+  { text: "Ideas", imgPath: "/images/ideas.svg" },
+  { text: "Concepts", imgPath: "/images/concepts.svg" },
+  { text: "Designs", imgPath: "/images/designs.svg" },
+  { text: "Code", imgPath: "/images/code.svg" },
+];
+
+const counterItems = [
+  { value: 2, suffix: "+", label: "Years of Experience" },
+  { value: 6, suffix: "+", label: "Satisfied Clients" },
+  { value: 10, suffix: "+", label: "Completed Projects" },
+  { value: 90, suffix: "%", label: "Client Retention Rate" },
+];
+
+const logoIconsList = [
+  {
+    imgPath: "/images/logos/company-logo-1.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-2.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-3.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-4.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-5.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-6.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-7.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-8.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-9.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-10.png",
+  },
+  {
+    imgPath: "/images/logos/company-logo-11.png",
+  },
+];
+
+const abilities = [
+  {
+    imgPath: "/images/seo.png",
+    title: "Quality Focus",
+    desc: "Delivering high-quality results while maintaining attention to every detail.",
+  },
+  {
+    imgPath: "/images/chat.png",
+    title: "Reliable Communication",
+    desc: "Keeping you updated at every step to ensure transparency and clarity.",
+  },
+  {
+    imgPath: "/images/time.png",
+    title: "On-Time Delivery",
+    desc: "Making sure projects are completed on schedule, with quality & attention to detail.",
+  },
+];
+
+const techStackImgs = [
+  {
+    name: "React Developer",
+    imgPath: "/images/logos/react.png",
+  },
+  {
+    name: "Python Developer",
+    imgPath: "/images/logos/python.svg",
+  },
+  // {
+  //   name: "Backend Developer",
+  //   imgPath: "/images/logos/node.png",
+  // },
+  {
+    name: "Interactive Developer",
+    imgPath: "/images/logos/three.png",
+  },
+  {
+    name: "Project Manager",
+    imgPath: "/images/logos/git.svg",
+  },
+];
+
+const techStackIcons = [
+  {
+    name: "React Developer",
+    modelPath: "/models/react_logo-transformed.glb",
+    scale: 1,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "UI/UX Designer",
+    modelPath: "/models/1.glb",
+    scale: 2,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Web Developer",
+    modelPath: "/models/node-transformed.glb",
+    scale: 5,
+    rotation: [0, -Math.PI / 2, 0],
+  },
+  {
+    name: "Interactive Developer",
+    modelPath: "/models/three.js-transformed.glb",
+    scale: 0.05,
+    rotation: [0, 0, 0],
+  },
+  {
+    name: "Project Manager",
+    modelPath: "/models/git-svg-transformed.glb",
+    scale: 0.05,
+    rotation: [0, -Math.PI / 4, 0],
+  },
+];
+
+const expCards = [
+  {
+    review:
+      "Ricky Rohaendi brought strong creativity and technical expertise to the team, significantly enhancing our Flutter-based frontend performance. His contributions have been instrumental in delivering smoother, faster, and more responsive user experiences.",
+    imgPath: "/images/flutter1.png",
+    logoPath: "/images/flutter2.png",
+    title: "Frontend Developer Internship",
+    date: "June - August 2023",
+    responsibilities: [
+      "Developed and maintained user-facing features for Flutter-based applications.",
+      "Collaborated closely with UI/UX designers to translate designs into seamless and intuitive mobile experiences.",
+      "Optimized application performance to ensure smooth, fast, and responsive user interactions across devices.",
+    ],
+  },
+  {
+    review:
+      "Ricky Rohaendi’s contributions as a UI/UX Designer have been outstanding. Using Figma, he approaches design challenges with a strong problem-solving mindset, creating intuitive and user-centered experiences.",
+    imgPath: "/images/FIGMA1.png",
+    logoPath: "/images/figma2.png",
+    title: "UI/UX Designer",
+    date: "November 2023 - November 2024",
+    responsibilities: [
+      "Led the end-to-end UI/UX design process, focusing on usability and scalable design systems in Figma.",
+      "Collaborated closely with developers to ensure smooth implementation of designs and consistency between UI and functionality.",
+      "Contributed to design systems and reusable components to maintain consistency across products.",
+    ],
+  },
+  // {
+  //   review:
+  //     "Ricky Rohaendi’s work as a Frontend UI Engineer on mobile applications brought a high level of quality and efficiency. He delivered well-crafted solutions that enhanced the overall user experience while aligning closely with product goals.",
+  //   imgPath: "/images/react1.png",
+  //   logoPath: "/images/react2.png",
+  //   title: "Front End UI Engineer",
+  //   date: "June 2025 - August 2025",
+  //   responsibilities: [
+  //     "Built cross-platform mobile apps using React Native, integrating with Appwrite's backend services.",
+  //     "Improved app performance and user experience through code optimization and testing.",
+  //     "Coordinated with the product team to implement features based on feedback.",
+  //   ],
+  // },
+  {
+    review:
+      "Ricky Rohaendi’s work as a Freelance Web Developer consistently delivered high-quality and efficient solutions. He created websites that enhanced user experience while aligning closely with client needs and business goals.",
+    imgPath: "/images/js1.png",
+    logoPath: "/images/js2.png",
+    title: "Freelance Web Developer",
+    date: "June 2025 - Present",
+    responsibilities: [
+      "Developed responsive and dynamic web applications using React tailored to client needs.",
+      "Optimized performance and user experience through efficient component design, testing, and best practices.",
+      "Collaborated closely with clients to implement features based on feedback and evolving project requirements.",
+    ],
+  },
+];
+
+const expLogos = [
+  {
+    name: "logo1",
+    imgPath: "/images/logo1.png",
+  },
+  {
+    name: "logo2",
+    imgPath: "/images/logo2.png",
+  },
+  {
+    name: "logo3",
+    imgPath: "/images/logo3.png",
+  },
+];
+
+const testimonials = [
+  {
+    name: "Esther Howard",
+    mentions: "@estherhoward",
+    review:
+      "Ricky helped us turn a pretty complex set of requirements into a clean and functional website. He was easy to communicate with and always found practical solutions when we hit challenges.",
+    imgPath: "/images/client1.png",
+  },
+  {
+    name: "Wade Warren",
+    mentions: "@wadewarren",
+    review:
+      "We had an outdated website and Ricky helped modernize it significantly. The result is much more user-friendly and performs better overall. Great attention to detail.",
+    imgPath: "/images/client3.png",
+  },
+  {
+    name: "Guy Hawkins",
+    mentions: "@guyhawkins",
+    review:
+      "Ricky was professional and consistent throughout the project. He delivered on time and was open to feedback, which made the whole process smooth.",
+    imgPath: "/images/client2.png",
+  },
+  {
+    name: "Marvin McKinney",
+    mentions: "@marvinmckinney",
+    review:
+      "Working with Ricky was straightforward and efficient. He improved our website structure and made it much easier for users to navigate.",
+    imgPath: "/images/client5.png",
+  },
+  {
+    name: "Floyd Miles",
+    mentions: "@floydmiles",
+    review:
+      "Ricky built a solid foundation for our e-commerce site. After launch, we noticed better performance and smoother user experience.",
+    imgPath: "/images/client4.png",
+  },
+  {
+    name: "Albert Flores",
+    mentions: "@albertflores",
+    review:
+      "Ricky understood what we needed and delivered a solution that worked well for both frontend and backend. Communication was clear and efficient.",
+    imgPath: "/images/client6.png",
+  },
+];
+
+const socialImgs = [
+  {
+    name: "insta",
+    imgPath: "/images/insta.png",
+    link: "https://instagram.com/rckyrrr", // Ganti dengan link Instagram Anda
+  },
+  {
+    name: "tiktok",
+    imgPath: "/images/Vector.png",
+    link: "https://tiktok.com/@rr_uid", // Ganti dengan link TikTok Anda
+  },
+  {
+    name: "x",
+    imgPath: "/images/x.png",
+    link: "https://x.com/ricky-rohaendi", // Ganti dengan link X (Twitter) Anda
+  },
+  {
+    name: "linkedin",
+    imgPath: "/images/linkedin.png",
+    link: "https://linkedin.com/in/ricky-rohaendi", // Ganti dengan link LinkedIn Anda
+  },
+];
+
+export {
+  words,
+  abilities,
+  logoIconsList,
+  counterItems,
+  expCards,
+  expLogos,
+  testimonials,
+  socialImgs,
+  techStackIcons,
+  techStackImgs,
+  navLinks,
+};
