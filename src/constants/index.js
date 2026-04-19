562a3282f21de42a2a5b792e4b606d8f1345cdab
rckyrrr <rickyrohaendi2@gmail.com>
2026-04-19 18:46:49 +0700

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -29,7 +29,7 @@ const words = [
 ];
 
 const counterItems = [
-  { value: 2, suffix: "+", label: "Years of Experience" },
+  { value: 1, suffix: "+", label: "Years of Experience" },
   { value: 6, suffix: "+", label: "Satisfied Clients" },
   { value: 10, suffix: "+", label: "Completed Projects" },
   { value: 90, suffix: "%", label: "Client Retention Rate" },
